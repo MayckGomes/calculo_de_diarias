d67a045f009c6cffbab5163e237a026e38acc2f3
Mayck Gomes <mayck.gomes2901@gmail.com>
2025-04-27 18:38:22 -0300

    ref: melhorado o fluxo fea: agora mostra os agregados

--- funcs.py
+++ funcs.py
@@ -2,8 +2,6 @@ from openpyxl import *
 
 lista_nomes = []
 
-colaborador = {}
-
 def mostrar_saldo(saldo_placas: dict):
     print("==" * 50)
     print("SALDOS")
@@ -22,113 +20,81 @@ def mostrar_saldo(saldo_placas: dict):
     print("=-" * 50)
     print(f"Total = {acumulador}")
 
-def buscar_colaborador(
-        nome: str,
-        lista_arquivos: list,
-        lista_tabelas: list
-        ):
+def buscar_colaborador(nome: str):
 
-    for index in range(len(lista_tabelas)):
+    arquivo = load_workbook("calculo_diarias.xlsx")
 
-        arquivo = load_workbook(lista_arquivos[index])
+    planilha = arquivo["Sheet1"]
 
-        planilha = arquivo[lista_tabelas[index]]
+    lista_motoristas = planilha["B"]
 
-        lista_motoristas = planilha["B"]
+    lista_ajudantes1 = planilha["E"]
 
-        lista_ajudantes1 = planilha["E"]
+    lista_ajudantes2 = planilha["F"]
 
-        lista_ajudantes2 = planilha["F"]
 
-        for motorista in range(3, len(lista_motoristas) + 1):
 
-            nome_motorista = planilha.cell(row=motorista, column=2).value
+    for motorista in range(3, len(lista_motoristas) + 1):
 
-            if nome_motorista == None:
-                continue
+        nome_motorista = planilha.cell(row=motorista, column=2).value
 
-            if nome in nome_motorista:
+        if nome_motorista == None:
+            continue
+
+        if nome in nome_motorista:
+            
+            if "AGREGADO" in nome_motorista:
+                placa = "AGREGADO"
+            else:
                 placa = planilha.cell(row=motorista, column=3).value
 
-                lista_nomes.append({nome_motorista: placa})
+            lista_nomes.append({nome_motorista: placa})
 
-        for ajudante1 in range(3, len(lista_ajudantes1) + 1):
 
-            nome_ajudante1 = planilha.cell(row=ajudante1, column=5).value
 
-            if nome_ajudante1 is None:
-                continue
+    for ajudante1 in range(3, len(lista_ajudantes1) + 1):
 
-            if nome in nome_ajudante1:
+        nome_ajudante1 = planilha.cell(row=ajudante1, column=5).value
+
+        if nome_ajudante1 is None:
+            continue
+
+        if nome in nome_ajudante1:
+            
+            if "AGREGADO" in planilha.cell(row=ajudante1, column=2).value: 
+
+                placa = "AGREGADO"
+            
+            else:
+
                 placa = planilha.cell(row=ajudante1, column=3).value
 
-                lista_nomes.append({nome_ajudante1: placa})
 
-        for ajudante2 in range(3, len(lista_ajudantes2) + 1):
+            lista_nomes.append({nome_ajudante1: placa})
 
-            nome_ajudante2 = planilha.cell(row=ajudante2, column=6).value
 
-            if nome_ajudante2 is None:
-                continue
+            
 
-            if nome in nome_ajudante2:
+    for ajudante2 in range(3, len(lista_ajudantes2) + 1):
+
+        nome_ajudante2 = planilha.cell(row=ajudante2, column=6).value
+
+        if nome_ajudante2 is None:
+            continue
+
+        if nome in nome_ajudante2:
+
+
+            if "AGREGADO" in planilha.cell(row=ajudante2, column=2).value: 
+
+                placa = "AGREGADO"
+            
+            else:
+
                 placa = planilha.cell(row=ajudante2, column=3).value
+            
 
-                lista_nomes.append({nome_ajudante2: placa})
+            lista_nomes.append({nome_ajudante2: placa})
 
     return lista_nomes
-
-def verificar_lista_nomes(lista: list):
-
-    global colaborador
-
-    if len(lista_nomes) == 0:
-        colaborador = {}
-        print("Não foi encontrado um colaborador com esse nome!")
-
-    elif len(lista_nomes) == 1:
-
-        nome = list(lista_nomes[0].keys())[0]
-        placa = list(lista_nomes[0].values())[0]
-
-        colaborador = {nome: placa}
-
-        lista_nomes.clear()
-
-        print("\n")
-        print(f"nome: {nome}, placa: {placa}")
-
-    if len(lista_nomes) > 1:
-
-        while True:
-
-            try:
-
-                print("=" * 100)
-
-                print("Existe mais de um colaborador com este nome, qual seria o certo: ")
-                print("\n")
-                for id_nome in range(len(lista_nomes)):
-                    nomebusca = list(lista_nomes[id_nome].keys())[0]
-
-                    print(f"id: {id_nome}, nome: {nomebusca}")
-
-                input_id = int(input("id do colaborador: "))
-
-                nome = list(lista_nomes[input_id].keys())[0]
-                placa = list(lista_nomes[input_id].values())[0]
-
-                colaborador = {nome: placa}
-                lista_nomes.clear()
-
-                print(f"nome: {nome}, placa: {placa}")
-
-                break
-
-            except:
-                print("=-" * 50)
-                print("id invalido")
-                print("=-" * 50)
-                
-    return colaborador
-                            
+    
--- main.py
+++ main.py
@@ -22,18 +22,13 @@ saldo_placas = {}
 
 lista_nomes = []
 
-lista_arquivos = []
-
-lista_tabelas = []
-
 while True:
     os.system("cls")
     print("=="*50)
     print("Menu")
-    print("1 - Adicionar um arquivo")
-    print("2 - Ver um colaborador e adicionar um saldo")
-    print("3 - Apenas ver um colaborador")
-    print("4 - Mostrar saldos")
+    print("1 - Ver um colaborador e adicionar um saldo")
+    print("2 - Apenas ver um colaborador")
+    print("3 - Mostrar saldos")
 
     try:
         decisao_menu = int(input("Digite a sua opção: "))
@@ -49,33 +44,66 @@ while True:
         match decisao_menu:
 
             case 1:
-                caminho_arquivo = input("Cole aqui o caminho para o arquivo, mais o tipo (C:/usuario/planilha.xlsx) :")
-
-                lista_arquivos.append(caminho_arquivo)
-
-                print("Caminho adicionado com sucesso!\n")
-
-                nome_tabela = input("Agora digite o nome da tabela do arquivo: ")
-
-                lista_tabelas.append(nome_tabela)
-
-                print("Arquivo adicionado com sucesso")
-                input()
-
-            case 2:
 
                 nome = input("digite o nome do colaborador(000 para parar): ")
 
                 if nome == "000":
                     break
                 
-                lista_nomes = buscar_colaborador(
-                    nome= nome.upper(),
-                    lista_arquivos=lista_arquivos,
-                    lista_tabelas=lista_tabelas
-                )
+                lista_nomes = buscar_colaborador(nome= nome.upper())
 
-                colaborador_atual = verificar_lista_nomes(lista_nomes)
+
+                if len(lista_nomes) == 0:
+        
+                    print("Não foi encontrado um colaborador com esse nome!")
+
+                elif len(lista_nomes) == 1:
+
+                    nome = list(lista_nomes[0].keys())[0]
+                    placa = list(lista_nomes[0].values())[0]
+
+                    colaborador_atual = {nome: placa}
+
+                    lista_nomes.clear()
+
+                    print("\n")
+                    print(f"nome: {nome}, placa: {placa}")
+
+                if len(lista_nomes) > 1:
+
+                    while True:
+
+                        try:
+
+                            print("=" * 100)
+
+                            print("Existe mais de um colaborador com este nome, qual seria o certo: ")
+                            print("\n")
+                            
+                            for id_nome in range(len(lista_nomes)):
+
+                                nomebusca = list(lista_nomes[id_nome].keys())[0]
+
+                                print(f"id: {id_nome}, nome: {nomebusca}")
+
+                            input_id = int(input("id do colaborador: "))
+
+                            nome = list(lista_nomes[input_id].keys())[0]
+                            placa = list(lista_nomes[input_id].values())[0]
+
+                            colaborador_atual = {nome: placa}
+                            lista_nomes.clear()
+
+                            print(f"nome: {nome}, placa: {placa}")
+
+                            break
+
+                        except:
+                            print("=-" * 50)
+                            print("id invalido")
+                            print("=-" * 50)
+                            
+
 
                 while True:    
                     decisao_saldo = input("Deseja colocar um saldo nesta placa?(S/N): ").upper()
@@ -109,23 +137,67 @@ while True:
                     input()
 
 
-            case 3:
+            case 2:
                 
                 nome = input("digite o nome do colaborador(000 para parar): ")
 
                 if nome == "000":
                     break
                 
-                lista_nomes = buscar_colaborador(
-                    nome= nome.upper(),
-                    lista_arquivos=lista_arquivos,
-                    lista_tabelas=lista_tabelas
-                )
+                lista_nomes = buscar_colaborador(nome= nome.upper())
 
-                colaborador_atual = verificar_lista_nomes(lista_nomes)
+                if len(lista_nomes) == 0:
+            
+                    print("Não foi encontrado um colaborador com esse nome!")
+
+                elif len(lista_nomes) == 1:
+
+                    nome = list(lista_nomes[0].keys())[0]
+                    placa = list(lista_nomes[0].values())[0]
+
+                    colaborador = {nome: placa}
+
+                    lista_nomes.clear()
+
+                    print("\n")
+                    print(f"nome: {nome}, placa: {placa}")
+
+                if len(lista_nomes) > 1:
+
+                    while True:
+
+                        try:
+
+                            print("=" * 100)
+
+                            print("Existe mais de um colaborador com este nome, qual seria o certo: ")
+                            print("\n")
+                            for id_nome in range(len(lista_nomes)):
+                                nomebusca = list(lista_nomes[id_nome].keys())[0]
+
+                                print(f"id: {id_nome}, nome: {nomebusca}")
+
+                            input_id = int(input("id do colaborador: "))
+
+                            nome = list(lista_nomes[input_id].keys())[0]
+                            placa = list(lista_nomes[input_id].values())[0]
+
+                            colaborador = {nome: placa}
+                            lista_nomes.clear()
+
+                            print(f"nome: {nome}, placa: {placa}")
+
+                            break
+
+                        except:
+                            print("=-" * 50)
+                            print("id invalido")
+                            print("=-" * 50)
+                            
+                
                 input()
 
-            case 4:
+            case 3:
                 mostrar_saldo(saldo_placas)
                 input()
 
